a06cc6618c42c1837af1de38cf61486c7c7f306f
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-08-20 16:28:15 +0530

    Update users page

--- src/init.js
+++ src/init.js
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/main.css";
 import NavBar from "./components/NavBar/NavBar";
 import ProfilePage from "./pages/Account/ProfilePage";
+import ViewDrivers from "./pages/Users/Drivers";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 //   <div><h1>Init.js</h1></div>
   <BrowserRouter>
   <NavBar />
     <Routes>
-      <Route path="/" element={<ProfilePage />}></Route>
+      <Route path="/" element={<ViewDrivers />}></Route>
     </Routes>
   </BrowserRouter>
 );
