4b00c8934bfe964017782bbce0898f3cbe534fc9
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-08-20 11:54:22 +0530

    Add account profile

--- src/pages/Account/ProfilePage.js
+++ src/pages/Account/ProfilePage.js
@@ -14,7 +14,7 @@ class AccountProfilePage extends React.Component {
               <p
                 tabIndex={0}
                 role="button"
-                className="text-base bg-orange bg-opacity-80 px-4 py-2 rounded focus:outline-none focus:ring-1 pr-2 focus:ring-offset-1 focus:ring-orange font-medium mr-10 leading-4 text-black"
+                className="text-base bg-orange bg-opacity-80 px-4 py-2 rounded focus:outline-none focus:ring-1 pr-2 focus:ring-offset-1 focus:ring-orange font-medium mr-10 leading-4 text-white"
               >
                 Profile
               </p>
@@ -55,20 +55,8 @@ class AccountProfilePage extends React.Component {
               </p>
             </div>
 
-            <div className="md:flex items-center mt-12">
-              <div className="md:w-72 flex flex-col">
-                <label className="text-base font-semibold leading-none text-gray-800">
-                  Name
-                </label>
-                <input
-                  tabIndex={0}
-                  arial-label="Please input name"
-                  type="name"
-                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
-                  placeholder="Please input  name"
-                />
-              </div>
-              <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+            <div className="md:flex items-center mt-8">
+              <div className="md:w-72 flex flex-col md:mt-0 mt-4">
                 <label className="text-base font-semibold leading-none text-gray-800">
                   Email Address
                 </label>
@@ -76,60 +64,155 @@ class AccountProfilePage extends React.Component {
                   tabIndex={0}
                   arial-label="Please input email address"
                   type="name"
-                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
                   placeholder="Please input email address"
                 />
               </div>
             </div>
-            <div className="md:flex items-center mt-8">
-              <div className="md:w-72 flex flex-col">
+            <div className="md:flex items-center mt-4">
+              <div className="md:w-72 flex flex-col md:mt-0 mt-4">
                 <label className="text-base font-semibold leading-none text-gray-800">
-                  Company name
+                  Password
                 </label>
                 <input
                   tabIndex={0}
-                  role="input"
-                  arial-label="Please input company name"
-                  type="name"
-                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 "
-                  placeholder="Please input company name"
+                  arial-label="Please input password"
+                  type="password"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input password"
                 />
               </div>
               <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Confirm Password
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input re-input password"
+                  type="password"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input re-input password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center w-full">
+              <button className="mt-4 text-base font-semibold leading-none text-white py-4 px-10 bg-green rounded hover:bg-dark_green focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
+                Change Password
+              </button>
+            </div>
+            <div className="md:flex items-center mt-8">
+              <div className="md:w-72 flex flex-col md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Business Name
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input business name"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input business name"
+                />
+              </div>
+              <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Registration Number
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input registration number"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input registration number"
+                />
+              </div>
+            </div>
+            <div className="md:flex items-center mt-4">
+              <div className="md:w-72 flex flex-col md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Address
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input address"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input address"
+                />
+              </div>
+              <div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Registration Certificate
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input registration certificate"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input registration certificate"
+                />
+              </div>
+            </div>
+            <div className="md:flex items-center mt-4">
+              <div className="md:w-40 flex flex-col md:mt-0 mt-4">
                 <label className="text-base font-semibold leading-none text-gray-800">
                   Country
                 </label>
                 <input
                   tabIndex={0}
-                  arial-label="Please input country name"
+                  arial-label="Please input country"
                   type="name"
-                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
-                  placeholder="Please input country name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input country"
                 />
               </div>
-            </div>
-            <div>
-              <div className="w-full flex flex-col mt-8">
+              <div className="md:w-40 flex flex-col md:ml-6 md:mt-0 mt-4">
                 <label className="text-base font-semibold leading-none text-gray-800">
-                  Message
+                  City
                 </label>
-                <textarea
+                <input
                   tabIndex={0}
-                  aria-label="leave a message"
-                  role="textbox"
+                  arial-label="Please input city"
                   type="name"
-                  className="h-36 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none"
-                  defaultValue={""}
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input city"
+                />
+              </div>
+              <div className="md:w-40 flex flex-col md:ml-6 md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  State
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please state"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please state"
+                />
+              </div>
+              <div className="md:w-40 flex flex-col md:ml-6 md:mt-0 mt-4">
+                <label className="text-base font-semibold leading-none text-gray-800">
+                  Postal Code
+                </label>
+                <input
+                  tabIndex={0}
+                  arial-label="Please input postal code"
+                  type="name"
+                  className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-orange mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+                  placeholder="Please input postal code"
                 />
               </div>
             </div>
-            <p className="text-xs leading-3 text-gray-600 mt-4">
-              By clicking submit you agree to our terms of service, privacy
-              policy and how we use data as stated
+
+            <p className="text-xs leading-3 text-gray-600 mt-4 md:w-[44rem]">
+              By clicking update and verify, you have provided complete and
+              accurate information on the Transpik online registration form, and
+              that you will immediately update your account profile of any
+              changes to that information.
             </p>
             <div className="flex items-center w-full">
-              <button className="mt-9 text-base font-semibold leading-none text-white py-4 px-10 bg-green rounded hover:bg-dark_green focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
-                SUBMIT
+              <button className="mt-4 text-base font-semibold leading-none text-white py-4 px-10 bg-green rounded hover:bg-dark_green focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none">
+                Update and Verify
               </button>
             </div>
           </div>
