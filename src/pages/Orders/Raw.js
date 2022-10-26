575b42f42f43eaf2b4b140464c72c2b16709a879
R4JxF60 <rajithakumaraprog@gmail.com>
2022-10-26 05:44:46 +0530

    add Raw component

---
+++ src/pages/Orders/Raw.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+const accessToken = window.localStorage.getItem('accessToken');
+
+function Raw(props) {
+    const [assignDriver, setAssignDriver] = useState("None");
+    
+
+    const onChangeAssignDriver = (event) => {
+        setAssignDriver(event.target.value);
+    }
+
+    const moveToDelivering = (event) => {
+        axios({
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Token': accessToken
+                },
+                    url: 'https://transpikapi.onrender.com/orders/stages/processing',
+                    mode: 'cors',
+                    withCredentials: true,
+                    data: {
+                    order_id: event.target.value,
+                    driver_id: assignDriver
+                }
+                }).then(response => {
+                //
+            }).catch(error => {
+            console.log(error);
+            })
+    }
+
+    return (
+        <tr>
+            <td className="pl-2 pr-4 py-6">
+            <div className="font-medium text-gray-800">{props.order._id}</div>
+            </td>
+            <td className="pl-2 pr-4 py-6">
+            <div className="font-medium text-gray-800">
+                {props.order.pickup_location.address}
+            </div>
+            </td>
+            <td className="pl-2 pr-4 py-6">
+            <div className="font-medium text-gray-800">
+                {props.order.delivery_location.address}   
+            </div>
+            </td>
+            <td className="pl-2 pr-4 py-6">
+            <div className="flex justify-center items-center w-full">
+                <div className="status-cold">{props.order.payment_status}</div>
+            </div>
+            </td>
+            
+            <td>
+            <div className="flex justify-center items-center w-full">
+                <select
+                value={assignDriver}
+                onChange={onChangeAssignDriver}
+                className="text-base leading-none text-gray-900 p-1 focus:oultine-none focus:border-orange bg-gray-100 border rounded border-gray-200"
+                >
+                <option value="None">None</option>
+                {
+                    props.drivers.map(driver => {
+                        return (
+                            (driver.postal_code == props.order.delivery_location.postal_code) ?
+                            <option key={driver._id} value={driver._id}>{driver.initials+driver.last_name}</option> : null
+                        );
+                    })
+                }
+                </select>
+            </div>
+            </td>
+            <td className="pl-2 pr-4 py-6">
+            <button onClick={moveToDelivering}>Move to delivering</button>
+            </td>
+        </tr>
+    );
+}
+
+export default Raw;
