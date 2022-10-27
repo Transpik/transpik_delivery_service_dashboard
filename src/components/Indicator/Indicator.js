767e4ef9bd5141d405d946bc0e29f5da08b06956
R4JxF60 <rajithakumaraprog@gmail.com>
2022-10-27 21:38:34 +0530

    fix indicator position

--- src/components/Indicator/Indicator.js
+++ src/components/Indicator/Indicator.js
@@ -3,7 +3,7 @@ import React from "react";
 function Indicator(props) {
     
     return (
-        <div className="fixed top-4 flex justify-center">
+        <div className="flex justify-center w-full">
             <div className="bg-white w-28 p-5 rounded-lg">
                 <div>{props.message}</div>
             </div>
